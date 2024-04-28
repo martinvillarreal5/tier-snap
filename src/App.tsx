302d67cbd7466c8d64f87ac5d35199f022955125
Martin Villarreal <martinvictorvillarreal@gmail.com>
2024-04-28 18:28:10 -0300

    update app & index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import TierContainer from './components/tier-table/TierContainer';
 
 function App() {
   return (
-    <>
-      <h1>TIER SNAP</h1>
-    </>
+    <div className="container mx-auto flex min-h-screen flex-col justify-center">
+      <h1 className="mb-3 text-center text-white">TIER SNAP</h1>
+      <TierContainer />
+    </div>
   );
 }
 
