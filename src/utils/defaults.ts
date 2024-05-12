7ff330482a6a4cae95c267b3a3753f2c53509c89
Martin Villarreal <martinvictorvillarreal@gmail.com>
2024-05-12 20:26:19 -0300

    update defaults.ts

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -8,31 +8,45 @@ export const defaultColorOptions: ColorOption[] = [
   { color: '#ffff7f' },
   { color: '#bfff7f' },
   { color: '#7fff7f' },
+  { color: '#7fffff' },
+  { color: '#7fbfff' },
+  { color: '#7f7fff' },
+  { color: '#ff7fff' },
+  { color: '#bf7fbf' },
 ];
 
 export const defaultTableConfig: TierTableConfig = {
   rowColor: '#1a1a17',
   snapColor: '#242222',
+  title: 'Example Tier Table',
 };
 
-export const defaultTemplateTable: TierTable = {
-  config: defaultTableConfig,
-  rows: [
-    {
-      ...defaultColorOptions[0],
-      title: 'S',
-      snaps: [
-        {
-          title: 'example snap',
-        },
-      ],
-    },
-    { ...defaultColorOptions[1], title: 'A', snaps: [] },
-    { ...defaultColorOptions[2], title: 'B', snaps: [] },
-    { ...defaultColorOptions[3], title: 'C', snaps: [] },
-    { ...defaultColorOptions[4], title: 'D', snaps: [] },
-    { ...defaultColorOptions[5], title: 'E', snaps: [] },
-  ],
+export const getExampleItem = (rowId: string) => {
+  return {
+    title: 'example',
+    id: crypto.randomUUID(),
+    rowId: rowId,
+  };
+};
+
+export const getDefaultTemplateTable = (): TierTable => {
+  const rowCeroId = crypto.randomUUID();
+
+  return {
+    config: defaultTableConfig,
+    rows: [
+      {
+        ...defaultColorOptions[0],
+        id: rowCeroId,
+        title: 'S',
+      },
+      { ...defaultColorOptions[1], id: crypto.randomUUID(), title: 'A' },
+      { ...defaultColorOptions[2], id: crypto.randomUUID(), title: 'B' },
+      { ...defaultColorOptions[3], id: crypto.randomUUID(), title: 'C' },
+      { ...defaultColorOptions[4], id: crypto.randomUUID(), title: 'D' },
+      { ...defaultColorOptions[5], id: crypto.randomUUID(), title: 'E' },
+    ],
+  };
 };
 
 export const defaultTierPallete: TierPallete = {
