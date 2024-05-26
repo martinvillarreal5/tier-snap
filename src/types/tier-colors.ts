e131251f1721c23c4414ddbc71cbf0958e0bc990
Martin Villarreal <martinvictorvillarreal@gmail.com>
2024-05-26 14:15:17 -0300

    add TierColor enum

---
+++ src/types/tier-colors.ts
@@ -0,0 +1,14 @@
+export enum TierColor {
+  RED = '#ff7f7f',
+  ORANGE = '#ffbf7f',
+  AMBER = '#ffdf7f',
+  YELLOW = '#ffff7f',
+  LIME = '#bfff7f',
+  GREEN = '#7fff7f',
+  CYAN = '#7fffff',
+  SKY = '#7fbfff',
+  INDIGO = '#7f7fff',
+  PINK = '#ff7fff',
+  MAGENTA = '#bf7fbf',
+  WHITE = '#fafafa',
+}
