c9a4a52d64b900809600f41d884459c6dee3b53b
Martin Villarreal <martinvictorvillarreal@gmail.com>
2024-05-26 18:32:59 -0300

    re-add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from './ThemeProvider';
 import { TierContainer } from './components/TierContainer';
 
 function App() {
   return (
-    <div className="container mx-auto flex min-h-screen flex-col pb-24 pt-8">
-      <TierContainer />
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="container mx-auto flex min-h-screen flex-col pb-24 pt-8">
+        <TierContainer />
+      </div>
+    </ThemeProvider>
   );
 }
 
