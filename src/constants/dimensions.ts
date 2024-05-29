5baab726d306377d28f31c9a5ebf8d71aefdb936
Martin Villarreal <martinvictorvillarreal@gmail.com>
2024-05-29 18:16:03 -0300

    add dimensions

---
+++ src/constants/dimensions.ts
@@ -0,0 +1,9 @@
+export class Dimensions {
+  static readonly ITEMS_PER_ROW = 7;
+  static readonly ITEM_SIZE = 6.5;
+  static readonly ROW_HEIGHT = 7;
+
+  private constructor() {
+    //hidden
+  }
+}
