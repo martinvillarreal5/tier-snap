b38c3020d597b746504ec4a2a720a85c0d21aeb7
Martin Villarreal <martinvictorvillarreal@gmail.com>
2024-05-29 18:19:52 -0300

    update TierTooling to use Button

--- src/components/TierTooling.tsx
+++ src/components/TierTooling.tsx
@@ -1,17 +1,15 @@
 import { useTierStore } from '@/hooks/useTierStore';
 import { NewRowModalButton } from './tooling/NewRowModalButton';
 import { TriangleAlert as TriangleAlertIcon } from 'lucide-react';
+import { Button } from './ui/button';
 
 export function TierTooling() {
   const reset = useTierStore.use.reset();
   return (
     <div className="flex grow flex-col items-center gap-2 bg-zinc-900 p-4 text-white">
-      <button
-        type="button"
-        className="flex cursor-pointer gap-2 rounded bg-zinc-700 px-4 py-2 duration-300 hover:bg-zinc-800"
-        onClick={() => reset()}>
+      <Button type="button" onClick={() => reset()} variant="secondary" className="gap-2">
         <TriangleAlertIcon className="text-[#ff7f7f] " /> Reset
-      </button>
+      </Button>
       <NewRowModalButton />
     </div>
   );
