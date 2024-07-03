5e48846d3a73b8e70ba862f69c03ec32ea431694
Martin Villarreal <martinvictorvillarreal@gmail.com>
2024-07-03 20:21:57 -0300

    update TierTooling

--- src/components/TierTooling.tsx
+++ src/components/TierTooling.tsx
@@ -1,16 +1,15 @@
-import { useTierStore } from '@/hooks/useTierStore';
 import { NewRowModalButton } from './tooling/NewRowModalButton';
-import { TriangleAlert as TriangleAlertIcon } from 'lucide-react';
-import { Button } from './ui/button';
+import { AlbumSection } from './tooling/AlbumSection';
+import { ResetTierButton } from './tooling/ResetTierButton';
 
 export function TierTooling() {
-  const reset = useTierStore.use.reset();
   return (
     <div className="flex w-full flex-col items-center gap-2 bg-zinc-900 p-4 text-white">
-      <Button type="button" onClick={() => reset()} variant="secondary" className="gap-2">
-        <TriangleAlertIcon className="text-[#ff7f7f] " /> Reset
-      </Button>
-      <NewRowModalButton />
+      <AlbumSection />
+      <div className="flex flex-row gap-2">
+        <ResetTierButton />
+        <NewRowModalButton />
+      </div>
     </div>
   );
 }
