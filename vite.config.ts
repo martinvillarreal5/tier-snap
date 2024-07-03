7154d796d225fb7247eee4304453d4ebc1023f53
Martin Villarreal <martinvictorvillarreal@gmail.com>
2024-07-03 20:20:26 -0300

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 
@@ -10,8 +10,26 @@ const resolveConfig: ResolveConfig = {
   alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
 };
 
+const cherryPickedKeys = ['SPOTIFY_API_CLIENT_ID', 'SPOTIFY_API_CLIENT_SECRET'];
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: resolveConfig,
+export default defineConfig(({ command, mode }) => {
+  try {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+      define: {
+        'process.env.SPOTIFY_API_CLIENT_ID': JSON.stringify(env.SPOTIFY_API_CLIENT_ID),
+        'process.env.SPOTIFY_API_CLIENT_SECRET': JSON.stringify(env.SPOTIFY_API_CLIENT_SECRET),
+      },
+      plugins: [react()],
+      resolve: resolveConfig,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+  return {
+    plugins: [react()],
+    resolve: resolveConfig,
+  };
 });
